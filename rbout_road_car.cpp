65d3eacb8c06d1026a63def4c2d9a7c1ac464282
vero-dav-vero <100243585+vero-dav-vero@users.noreply.github.com>
2022-08-07 16:11:42 +0200

    Update rbout_road_car_1.cpp
    Abbiamo fatto delle modifiche, mancano le funzioni ma sono in corso d'opera. Abbiamo fatto delle considerazioni su cui poi vorremmo parlare

--- rbout_road_car.cpp
+++ rbout_road_car.cpp
@@ -1,11 +1,20 @@
 #include <random>
-#include<vector>
+#include <vector>
 #include <cmath>
 
-int n_roads;
-std::default_random_engine gen;
-std::binomial_distribution<int> binomial(0.5); //è stato un tentativo per mettere gli ingressi ma mi sa che devo essere un po' più specifica (devo fare come in root il set seed o una cosa analoga?)
-std::uniform_int_distribution<int> out(1, n_roads); //poi la potremo cambiare con qualsiasi distribuzione preferiamo, potrei sempre aver sbagliato la sintassi obv
+int n_roads; //saranno quattro poi di base, Simo è rompiballe (se lo dice da solo)
+double v_max_rbout;
+double v_max_road;
+
+double exit_angle() //funzione che mi dà l'uscita espressa come angolo
+{
+  std::random_device seed_1; //è il seed siamo felici
+  std::default_random_engine random_1(seed_1());
+  std::uniform_int_distribution<int> numbers(1, n_roads);
+  int x = numbers(random_1);
+  double y = (2 * M_PI / n_roads) * x;
+  return y;
+}
 
 
 class car
@@ -13,10 +22,10 @@ class car
   private:
     double r_;
     double theta_;
-    double omega_;
-    int exit_;
+    double v_; //nel costruttire poi andrebbe chiamata la tangente nel caso si volesse fare
+    double exit_; //vedi dopo
   public:
-    car(double r = 0., double theta = 0., double omega = 0., int exit = out(gen)) : r_{r}, theta_{theta}, omega_{omega}, exit_{exit} {}
+    car(double r = 0., double theta = 0., double v = 0., double exit = exit_angle()) : r_{r}, theta_{theta}, v_{v}, exit_{exit} {}
 
 };
 
@@ -36,13 +45,26 @@ class road
 {
   private:
     double lenght_;
-    auto angle = (2*M_PI)/n_roads;
-    int entrance_flow_;
+    double angle = (2 * M_PI)/n_roads;
+    double entrance_par_;
     std::vector<car> car_in;
     std::vector<car> car_out;
   public:
-    road(double len = rbout::rad, int in_f = binomial(gen)) : lenght_{len}, entrance_flow_{in_f} {}
+    road(double len = 0., double en_par = 0.) : lenght_{len}, entrance_par_{en_par} {}
 
+    void newcar(car const& C)
+    {
+      std::random_device seed_2;
+      std::default_random_engine random_2(seed_2);
+      std::uniform_real_distribution<double> parameters (0., 1.);
+      double par = parameters(random_2);
+      if (par < road.en_par) //non prende la variabile e non so come fare
+      {
+        car_in.push_back(C); //in fase di progettanzione ma ci siamo (direi)
+
+      }
+      
+    }
     /*qui ci saranno solo le dichiarazioni delle funzioni membro 
     (se non erro) mentre le definizioni saranno da un'altra parte
     in un altro file cpp (mi sono basata su vaghi e lontani ricordi
