ce0c028fd4ec661647adbf64af2dfa17a559b3df
SimoNaglio <100242144+SimoNaglio@users.noreply.github.com>
2022-08-19 16:44:57 +0200

    Add files via upload

---
+++ simo prova tangente che non fa/rbout.hpp
@@ -0,0 +1,29 @@
+#ifndef RBOUT_H
+#define RBOUT_H
+
+#include"car.hpp"
+
+
+
+class rbout {
+ private:
+  double radius_;
+  std::vector<car> car_rbout;
+
+ public:
+  rbout(double rad = 0.) : radius_{rad} {}
+  double rad();
+  std::vector<car> carrbout();
+
+  std::size_t size_rbout();
+  bool empty_rbout();
+
+  void newcar_rbt(double);
+
+  void erase_rbt();
+
+  int transfer_rbt();
+
+  void evolve_rbt();
+};
+#endif
