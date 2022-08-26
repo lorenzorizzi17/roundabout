f90555ad6ecd40f84ca7994fdff681cab3c5e643
vero-dav-vero <100243585+vero-dav-vero@users.noreply.github.com>
2022-08-26 14:59:09 +0200

    Update main.cpp

--- Roundabout_folder/main.cpp
+++ Roundabout_folder/main.cpp
@@ -9,6 +9,10 @@ int main() {
   const double radius = 0.25 * display_height;
   int rate = 5;
   double minimum_angle = 10.;
+  if (minimum_angle >= 360.) {
+      throw std::runtime_error{
+          "Modificare l'angolo minimo tra gli oggetti car in rotonda."};
+    }
   int const fps = 60;
   rbout roundabout(4, radius);
 
