8a2abdc0b2a6fcbea01af66707ea51db0bc19ede
vero-dav-vero <100243585+vero-dav-vero@users.noreply.github.com>
2022-08-26 14:33:14 +0200

    Modifica al is_free

---
+++ Roundabout_folder/functions.hpp
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "road.hpp"
+
+bool can_generate(int const);
+
+int random_call(int const);
+
+double module360(double);
+
+bool is_free(double const, rbout &, double const);
+
+#endif
