bf21d333cd4e82d8a129361afeea6cc73d2de762
lorenzorizzi17 <102590961+lorenzorizzi17@users.noreply.github.com>
2022-08-29 11:46:10 +0200

    aggiunta const

--- Roundabout_folder/car.hpp
+++ Roundabout_folder/car.hpp
@@ -11,10 +11,10 @@ class car {
  public:
   car(double theta = 0., double t = 0., int exit = 0, bool can_I_enter = false)
       : theta_{theta}, t_{t}, exit_{exit}, can_I_enter_{can_I_enter} {}
-  double theta();
-  double t();
-  double exit();
-  bool can_I_enter();
+  double theta() const;
+  double t() const;
+  double exit() const;
+  bool can_I_enter() const;
   void can_I_enter_Y();
   void evolve_tplus();
   void evolve_tminus();
